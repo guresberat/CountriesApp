848e7cdfa7c253941af77288f5a03523fbde8c0c
Beratcan Güreş <guresberatcan@gmail.com>
2022-07-25 09:36:47 +0300

    Initial Commit

---
+++ app/src/main/java/com/guresberat/countriesapp/view/activity/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.guresberat.countriesapp.view.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        startActivity(Intent(this, MainActivity::class.java))
+    }
+}
